fc7e27d7f58204009f5aee6e5ef95e2c4f68d88c
pshsmc-emm <96363215+pshsmc-emm@users.noreply.github.com>
2022-09-12 08:57:54 +0800

    Update Ex02MuonMartinez.java

--- Q1Ex2/Ex02MuonMartinez.java
+++ Q1Ex2/Ex02MuonMartinez.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package q1ex2;
 import java.util.Scanner;
 
 /**
@@ -19,6 +20,8 @@ public class Ex02MuonMartinez {
         int lowerLimit = 0;
         int upperLimit = 10;
         int numOfGuesses = 3;
+        int range = 10;
+        //String retry = "y";
       
         //starting screen
         System.out.println("Welcome to Higher or Lower! What will you do?");
@@ -26,10 +29,37 @@ public class Ex02MuonMartinez {
         System.out.println("- Change settings");
         System.out.println("- End application");
         String userInput = scan.nextLine();
-
+        
+        //do {
         if(userInput.equalsIgnoreCase("start game")) {
-          System.out.println("You have " + numOfGuesses + " guess(es) left. What is your guess?");
+          int random = (int) Math.floor(Math.random()*range) + lowerLimit;  
+            
+          System.out.print("You have " + numOfGuesses + " guess(es) left. What is your guess? ");
           int userGuess = scan.nextInt();
+          
+          while(numOfGuesses > 1){
+              numOfGuesses -= 1;
+              
+              if(userGuess < random) {
+                  System.out.print("Guess higher! You have " + numOfGuesses +" guess(es) left. What is your guess? ");
+                  userGuess = scan.nextInt();
+              } else if (userGuess > random) {
+                  System.out.print("Guess lower! You have " + numOfGuesses +" guess(es) left. What is your guess? ");
+                   userGuess = scan.nextInt();
+              }
+            }
+          
+          if (userGuess == random){
+                System.out.println("You got it!");
+          } else {
+                System.out.println("You lost...");
+          }
+          
+          System.out.println("Play again (y/n)? ");
+          String retryGame = scan.nextLine();
+          
+          System.out.println("test");
+          
           
         } else if(userInput.equalsIgnoreCase("change settings")) {
           System.out.println("What is the lower limit of the random number?");
@@ -42,12 +72,14 @@ public class Ex02MuonMartinez {
           upperLimit = scan.nextInt();
           System.out.print("Guesses: ");
           numOfGuesses = scan.nextInt();
+                  
 
         } else if(userInput.equalsIgnoreCase("end application")) {
-          System.out.println("end");
+          System.out.println("Thank you for playing!");
           
         } else {
           System.out.println("Please input an appropriate response.");
         }
-    }
+   // } while(retry.equalsIgnoreCase("y"));
+  }
 }
